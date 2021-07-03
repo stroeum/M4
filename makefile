stroeum/M4

commit e33f9d4850e004e03b0cdda77cd0326075463327

Added gradpswitch checks throughout source code

src/MyPDEs.c
@@ -1139,9 +1139,11 @@ PetscErrorCode FormIntermediateFunction(PetscReal ****u, Vec V, void *ctx)
 				B[2]        = u[k][j][i][d.B[2]];        // Bz
 				
 				// Intermediate calculations
-				dpe.dx = D1.x[0]*u[k][j][id.x[0]][d.pe] + D1.x[1]*u[k][j][id.x[1]][d.pe] + D1.x[2]*u[k][j][id.x[2]][d.pe]; //dpe.dx 
-				dpe.dy = D1.y[0]*u[k][id.y[0]][i][d.pe] + D1.y[1]*u[k][id.y[1]][i][d.pe] + D1.y[2]*u[k][id.y[2]][i][d.pe]; //dpe.dy 
-				dpe.dz = D1.z[0]*u[id.z[0]][j][i][d.pe] + D1.z[1]*u[id.z[1]][j][i][d.pe] + D1.z[2]*u[id.z[2]][j][i][d.pe]; //dpe.dz 
+				if (user->gradpswitch==1){
+					dpe.dx = D1.x[0]*u[k][j][id.x[0]][d.pe] + D1.x[1]*u[k][j][id.x[1]][d.pe] + D1.x[2]*u[k][j][id.x[2]][d.pe]; //dpe.dx 
+					dpe.dy = D1.y[0]*u[k][id.y[0]][i][d.pe] + D1.y[1]*u[k][id.y[1]][i][d.pe] + D1.y[2]*u[k][id.y[2]][i][d.pe]; //dpe.dy 
+					dpe.dz = D1.z[0]*u[id.z[0]][j][i][d.pe] + D1.z[1]*u[id.z[1]][j][i][d.pe] + D1.z[2]*u[id.z[2]][j][i][d.pe]; //dpe.dz 
+				}
 
 				for (m=0; m<3; m++) {
 					dB[m].dx = D1.x[0]*u[k][j][id.x[0]][d.B[m]] + D1.x[1]*u[k][j][id.x[1]][d.B[m]] + D1.x[2]*u[k][j][id.x[2]][d.B[m]]; //dB[m].dx 
@@ -1221,9 +1223,15 @@ PetscErrorCode FormIntermediateFunction(PetscReal ****u, Vec V, void *ctx)
 				}
 
 				// Gen Ohms Law
-				E[0] = -CrossP(ve,B,0) - dpe.dx/ne;
-				E[1] = -CrossP(ve,B,1) - dpe.dy/ne;
-				E[2] = -CrossP(ve,B,2) - dpe.dz/ne;
+				E[0] = -CrossP(ve,B,0);
+				E[1] = -CrossP(ve,B,1);
+				E[2] = -CrossP(ve,B,2);
+
+				if (user->gradpswitch==1){
+					E[0] += -dpe.dx/ne;
+					E[1] += -dpe.dy/ne;
+					E[2] += -dpe.dz/ne;
+				}
 
 				if(user->chemswitch==1){
 					E[0] += E_chem_S[0];
@@ -1701,9 +1709,11 @@ PetscErrorCode FormFunction(TS ts,PetscReal ftime,Vec U,Vec F,void *ctx)
 					dve[m].dy = D1.y[0]*v[k][id.y[0]][i][s.ve[m]] + D1.y[1]*v[k][id.y[1]][i][s.ve[m]] + D1.y[2]*v[k][id.y[2]][i][s.ve[m]]; //dve[m].dy 
 					dve[m].dz = D1.z[0]*v[id.z[0]][j][i][s.ve[m]] + D1.z[1]*v[id.z[1]][j][i][s.ve[m]] + D1.z[2]*v[id.z[2]][j][i][s.ve[m]]; //dve[m].dz 
 				}
-				dpe.dx = D1.x[0]*u[k][j][id.x[0]][d.pe] + D1.x[1]*u[k][j][id.x[1]][d.pe] + D1.x[2]*u[k][j][id.x[2]][d.pe]; //dpe.dx 
-				dpe.dy = D1.y[0]*u[k][id.y[0]][i][d.pe] + D1.y[1]*u[k][id.y[1]][i][d.pe] + D1.y[2]*u[k][id.y[2]][i][d.pe]; //dpe.dy 
-				dpe.dz = D1.z[0]*u[id.z[0]][j][i][d.pe] + D1.z[1]*u[id.z[1]][j][i][d.pe] + D1.z[2]*u[id.z[2]][j][i][d.pe]; //dpe.dz 
+				if (user->gradpswitch==1){
+					dpe.dx = D1.x[0]*u[k][j][id.x[0]][d.pe] + D1.x[1]*u[k][j][id.x[1]][d.pe] + D1.x[2]*u[k][j][id.x[2]][d.pe]; //dpe.dx 
+					dpe.dy = D1.y[0]*u[k][id.y[0]][i][d.pe] + D1.y[1]*u[k][id.y[1]][i][d.pe] + D1.y[2]*u[k][id.y[2]][i][d.pe]; //dpe.dy 
+					dpe.dz = D1.z[0]*u[id.z[0]][j][i][d.pe] + D1.z[1]*u[id.z[1]][j][i][d.pe] + D1.z[2]*u[id.z[2]][j][i][d.pe]; //dpe.dz 
+				}
 				
 				for (l=0; l<3; l++) {
 					dni[l].dx = D1.x[0]*u[k][j][id.x[0]][d.ni[l]] + D1.x[1]*u[k][j][id.x[1]][d.ni[l]] + D1.x[2]*u[k][j][id.x[2]][d.ni[l]]; //dni.dx 
@@ -1716,9 +1726,11 @@ PetscErrorCode FormFunction(TS ts,PetscReal ftime,Vec U,Vec F,void *ctx)
 						dvi[l][m].dz = D1.z[0]*u[id.z[0]][j][i][d.vi[l][m]] + D1.z[1]*u[id.z[1]][j][i][d.vi[l][m]] + D1.z[2]*u[id.z[2]][j][i][d.vi[l][m]]; //dvi[m].dz 
 					}
 					
-					dpi[l].dx = D1.x[0]*u[k][j][id.x[0]][d.pi[l]] + D1.x[1]*u[k][j][id.x[1]][d.pi[l]] + D1.x[2]*u[k][j][id.x[2]][d.pi[l]]; //dpi.dx 
-					dpi[l].dy = D1.y[0]*u[k][id.y[0]][i][d.pi[l]] + D1.y[1]*u[k][id.y[1]][i][d.pi[l]] + D1.y[2]*u[k][id.y[2]][i][d.pi[l]]; //dpi.dy 
-					dpi[l].dz = D1.z[0]*u[id.z[0]][j][i][d.pi[l]] + D1.z[1]*u[id.z[1]][j][i][d.pi[l]] + D1.z[2]*u[id.z[2]][j][i][d.pi[l]]; //dpi.dz 
+					if (user->gradpswitch==1){
+						dpi[l].dx = D1.x[0]*u[k][j][id.x[0]][d.pi[l]] + D1.x[1]*u[k][j][id.x[1]][d.pi[l]] + D1.x[2]*u[k][j][id.x[2]][d.pi[l]]; //dpi.dx 
+						dpi[l].dy = D1.y[0]*u[k][id.y[0]][i][d.pi[l]] + D1.y[1]*u[k][id.y[1]][i][d.pi[l]] + D1.y[2]*u[k][id.y[2]][i][d.pi[l]]; //dpi.dy 
+						dpi[l].dz = D1.z[0]*u[id.z[0]][j][i][d.pi[l]] + D1.z[1]*u[id.z[1]][j][i][d.pi[l]] + D1.z[2]*u[id.z[2]][j][i][d.pi[l]]; //dpi.dz 
+					}
 				}
 				
 				// CFL
@@ -1816,12 +1828,16 @@ PetscErrorCode FormFunction(TS ts,PetscReal ftime,Vec U,Vec F,void *ctx)
                 
 				// Eq 3-11: Ion momenta //
 				for (l=0; l<3; l++) {
-					f[k][j][i][d.vi[l][0]] = - (vi[l][0]*dvi[l][0].dx +vi[l][1]*dvi[l][0].dy +vi[l][2]*dvi[l][0].dz) + me/mi[l]*(E[0] +CrossP(vi[l],B,0) -dpi[l].dx/ni[l]);
-					f[k][j][i][d.vi[l][1]] = - (vi[l][0]*dvi[l][1].dx +vi[l][1]*dvi[l][1].dy +vi[l][2]*dvi[l][1].dz) + me/mi[l]*(E[1] +CrossP(vi[l],B,1) -dpi[l].dy/ni[l]);
-					f[k][j][i][d.vi[l][2]] = - (vi[l][0]*dvi[l][2].dx +vi[l][1]*dvi[l][2].dy +vi[l][2]*dvi[l][2].dz) + me/mi[l]*(E[2] +CrossP(vi[l],B,2) -dpi[l].dz/ni[l]);
+					f[k][j][i][d.vi[l][0]] = - (vi[l][0]*dvi[l][0].dx +vi[l][1]*dvi[l][0].dy +vi[l][2]*dvi[l][0].dz) + me/mi[l]*(E[0] +CrossP(vi[l],B,0);
+					f[k][j][i][d.vi[l][1]] = - (vi[l][0]*dvi[l][1].dx +vi[l][1]*dvi[l][1].dy +vi[l][2]*dvi[l][1].dz) + me/mi[l]*(E[1] +CrossP(vi[l],B,1);
+					f[k][j][i][d.vi[l][2]] = - (vi[l][0]*dvi[l][2].dx +vi[l][1]*dvi[l][2].dy +vi[l][2]*dvi[l][2].dz) + me/mi[l]*(E[2] +CrossP(vi[l],B,2);
 					if(user->gravswitch==1)
-						// f[k][j][i][d.vi[l][2]] += - PetscPowScalar(1+Z/rM,-2.0);   // Changed by Kellen to increase speed
 						f[k][j][i][d.vi[l][2]] += - 1/((1+Z/rM)*(1+Z/rM));
+					if(user->gradpswitch==1)
+						f[k][j][i][d.vi[l][0]] += -dpi[l].dx/ni[l];
+						f[k][j][i][d.vi[l][1]] += -dpi[l].dy/ni[l];
+						f[k][j][i][d.vi[l][2]] += -dpi[l].dz/ni[l];
+					}
 					if(user->chemswitch==1){
 						f[k][j][i][d.vi[l][0]] += mom_chem_S[l][0];
 						f[k][j][i][d.vi[l][1]] += mom_chem_S[l][1];
@@ -1837,14 +1853,21 @@ PetscErrorCode FormFunction(TS ts,PetscReal ftime,Vec U,Vec F,void *ctx)
 				// Eq 12-14: Equations of state for ions
 				// Read more about heat transfer (Schunk and Nagy Book). Appendix I. page 518
 				for (l=0; l<3; l++){
-					f[k][j][i][d.pi[l]] = - gama[l] * pi[l] * (dvi[l][0].dx + dvi[l][1].dy + dvi[l][2].dz) - (vi[l][0]*dpi[l].dx + vi[l][1]*dpi[l].dy + vi[l][2]*dpi[l].dz);
+					f[k][j][i][d.pi[l]] = - gama[l] * pi[l] * (dvi[l][0].dx + dvi[l][1].dy + dvi[l][2].dz);
+					if(user->gradpswitch==1){
+						f[k][j][i][d.pi[l]] += -(vi[l][0]*dpi[l].dx + vi[l][1]*dpi[l].dy + vi[l][2]*dpi[l].dz);
+					}
 					if(user->chemswitch==1){
 						f[k][j][i][d.pi[l]] += ene_chem_S[l] - ene_chem_L[l];
 					}
 				}
 				
 				// Eq 15: Equation of state for electrons
-				f[k][j][i][d.pe] = - gama[l] * pe * (dve[0].dx + dve[1].dy + dve[2].dz ) - ( ve[0]*dpe.dx + ve[1]*dpe.dy + ve[2]*dpe.dz);
+				f[k][j][i][d.pe] = - gama[l] * pe * (dve[0].dx + dve[1].dy + dve[2].dz );
+
+				if(user->gradpswitch==1){
+					f[k][j][i][d.pe] += -(ve[0]*dpe.dx + ve[1]*dpe.dy + ve[2]*dpe.dz);
+				}
 
 				if(user->chemswitch==1){
 					f[k][j][i][d.pe] += ene_chem_S[e] - ene_chem_L[e];

src/MyCtx.c
@@ -285,6 +285,8 @@ PetscErrorCode InitCtx(AppCtx *user, MonitorCtx *usrmnt)
     ierr = PetscOptionsGetInt(PETSC_NULL,PETSC_NULL,"-chem_switch",&user->chemswitch,PETSC_NULL);CHKERRQ(ierr);
     user->collswitch = 0;
     ierr = PetscOptionsGetInt(PETSC_NULL,PETSC_NULL,"-coll_switch",&user->collswitch,PETSC_NULL);CHKERRQ(ierr);
+    user->gradpswitch = 0;
+    ierr = PetscOptionsGetInt(PETSC_NULL,PETSC_NULL,"-gradp_switch",&user->gradpswitch,PETSC_NULL);CHKERRQ(ierr);
     user->gravswitch = 0;
     ierr = PetscOptionsGetInt(PETSC_NULL,PETSC_NULL,"-grav_switch",&user->gravswitch,PETSC_NULL);CHKERRQ(ierr);
     

src/main.c
@@ -44,6 +44,10 @@ int main(int argc,char **argv)
 		PetscPrintf(PETSC_COMM_WORLD,"\nGravity\t\t= ON\n");
 	else
 		PetscPrintf(PETSC_COMM_WORLD,"\nGravity\t\t= OFF\n");
+	if(user.gradpswitch==1)
+		PetscPrintf(PETSC_COMM_WORLD,"Pressure Grad\t= ON\n");
+	else
+		PetscPrintf(PETSC_COMM_WORLD,"Pressure Grad\t= OFF\n");
 	if(user.chemswitch==1)
 		PetscPrintf(PETSC_COMM_WORLD,"Chemistry\t= ON\n");
 	else

commit 0e13d135e806ba06659f603df976231adf92cc36

Fixed critical error in MyPDEs.c/1840 originally introduced in commit e33f9d4850e004e03b0cdda77cd0326075463327. This error occasionally caused the time step < 1 ps to throw.

makefile
@@ -70,4 +70,4 @@ $(BUILDDIR)/%.o: $(SRCDIR)/%.$(SRCEXT)
 
 clean_data:
 	@echo " Cleaning...";
-	$(RM) -r $(BUILDDIR) $(TARGETDIR) output/*
+	$(RM) -r $(BUILDDIR) $(TARGETDIR) output/* M4*.err M4*.out

src/MyChemistry.c
@@ -15,7 +15,7 @@
 #undef __FUNCT__
 #define __FUNCT__ "v1"
 PetscReal v1(void) {
-	return 0; //zzz
+	// return 0; //zzz
 	return 5.0e-7; //_s-1
 }
 
@@ -26,7 +26,7 @@ PetscReal v1(void) {
 #undef __FUNCT__
 #define __FUNCT__ "v2"
 PetscReal v2(PetscReal N , PetscReal Te) {
-	return 0; //zzz
+	// return 0; //zzz
 	// Ei in direct order //
 	PetscInt  Ni=58;
 	PetscReal me=9.1093e-31; //_kg
@@ -52,7 +52,7 @@ PetscReal v2(PetscReal N , PetscReal Te) {
 #undef __FUNCT__
 #define __FUNCT__ "v3"
 PetscReal v3(void) {
-	return 0; //zzz
+	// return 0; //zzz
 	return 2.0e-7; //_s-1
 }
 
@@ -63,7 +63,7 @@ PetscReal v3(void) {
 #undef __FUNCT__
 #define __FUNCT__ "v4"
 PetscReal v4(PetscReal N , PetscReal Te) {
-	return 0; //zzz
+	// return 0; //zzz
 	PetscReal K,E,v;
 	E = kB*Te/qe;
 	K = 9.0e-9*PetscPowScalar(E,0.7)*PetscExpScalar(-13.6/E); //_cm^3/_s
@@ -78,7 +78,7 @@ PetscReal v4(PetscReal N , PetscReal Te) {
 #undef __FUNCT__
 #define __FUNCT__ "v5"
 PetscReal v5(PetscReal N , PetscReal Te) {
-	return 0; //zzz
+	// return 0; //zzz
 	PetscReal K,v;
 	if (Te<1200) {
 		K = 1.95e-7*PetscPowScalar( 300/Te,0.7 ); //_cm^3/_s
@@ -96,7 +96,7 @@ PetscReal v5(PetscReal N , PetscReal Te) {
 #undef __FUNCT__
 #define __FUNCT__ "v6"
 PetscReal v6(PetscReal N , PetscReal Te) {
-	return 0; //zzz
+	// return 0; //zzz
 	PetscReal K, v;
 	K = 3.1e-7*PetscSqrtScalar(300/Te); //_cm^3/_s
 	v = N*K;
@@ -110,7 +110,7 @@ PetscReal v6(PetscReal N , PetscReal Te) {
 #undef __FUNCT__
 #define __FUNCT__ "v7"
 PetscReal v7(PetscReal N , PetscReal Te) {
-	return 0; //zzz
+	// return 0; //zzz
 	PetscReal K, v;
 	K = 3.71e-12*PetscPowScalar(250/Te,0.7); //_cm^3/_s
 	v = N*K;
@@ -124,7 +124,7 @@ PetscReal v7(PetscReal N , PetscReal Te) {
 #undef __FUNCT__
 #define __FUNCT__ "v8"
 PetscReal v8(PetscReal N) {
-	return 0; //zzz
+	// return 0; //zzz
 	PetscReal K, v;
 	K = 1.64e-10; //_cm^3/_s
 	v = N*K;
@@ -138,7 +138,7 @@ PetscReal v8(PetscReal N) {
 #undef __FUNCT__
 #define __FUNCT__ "v9"
 PetscReal v9(PetscReal N) {
-	return 0; //zzz
+	// return 0; //zzz
 	PetscReal K, v;
 	K = 9.6e-11; //_cm^3/_s
 	v = N*K;
@@ -152,7 +152,7 @@ PetscReal v9(PetscReal N) {
 #undef __FUNCT__
 #define __FUNCT__ "v10"
 PetscReal v10(PetscReal N) {
-	return 0; //zzz
+	// return 0; //zzz
 	PetscReal K, v;
 	K = 1.1e-9; //_cm^3/_s
 	v = N*K;

src/MyPDEs.c
@@ -1835,9 +1835,9 @@ PetscErrorCode FormFunction(TS ts,PetscReal ftime,Vec U,Vec F,void *ctx)
                 
 				// Eq 3-11: Ion momenta //
 				for (l=0; l<3; l++) {
-					fkji[d.vi[l][0]] = - (vi[l][0]*dvi[l][0].dx +vi[l][1]*dvi[l][0].dy +vi[l][2]*dvi[l][0].dz) + me/mi[l]*(E[0] +CrossP(vi[l],B,0));
-					fkji[d.vi[l][1]] = - (vi[l][0]*dvi[l][1].dx +vi[l][1]*dvi[l][1].dy +vi[l][2]*dvi[l][1].dz) + me/mi[l]*(E[1] +CrossP(vi[l],B,1));
-					fkji[d.vi[l][2]] = - (vi[l][0]*dvi[l][2].dx +vi[l][1]*dvi[l][2].dy +vi[l][2]*dvi[l][2].dz) + me/mi[l]*(E[2] +CrossP(vi[l],B,2));
+					fkji[d.vi[l][0]] = -(vi[l][0]*dvi[l][0].dx +vi[l][1]*dvi[l][0].dy +vi[l][2]*dvi[l][0].dz);
+					fkji[d.vi[l][1]] = -(vi[l][0]*dvi[l][1].dx +vi[l][1]*dvi[l][1].dy +vi[l][2]*dvi[l][1].dz);
+					fkji[d.vi[l][2]] = -(vi[l][0]*dvi[l][2].dx +vi[l][1]*dvi[l][2].dy +vi[l][2]*dvi[l][2].dz);
 					if(user->chemswitch==1){
 						fkji[d.vi[l][0]] += mom_chem_S[l][0];
 						fkji[d.vi[l][1]] += mom_chem_S[l][1];
@@ -1849,9 +1849,9 @@ PetscErrorCode FormFunction(TS ts,PetscReal ftime,Vec U,Vec F,void *ctx)
 						fkji[d.vi[l][2]] += el_coll[l][2];
 					}
 					if(user->gradpswitch==1){
-						fkji[d.vi[l][0]] += -dpi[l].dx/ni[l];
-						fkji[d.vi[l][1]] += -dpi[l].dy/ni[l];
-						fkji[d.vi[l][2]] += -dpi[l].dz/ni[l];
+						fkji[d.vi[l][0]] += me/mi[l]*(E[0] + CrossP(vi[l],B,0) - dpi[l].dx/ni[l]);
+						fkji[d.vi[l][1]] += me/mi[l]*(E[1] + CrossP(vi[l],B,1) - dpi[l].dy/ni[l]);
+						fkji[d.vi[l][2]] += me/mi[l]*(E[2] + CrossP(vi[l],B,2) - dpi[l].dz/ni[l]);
 					}
 					if(user->gravswitch==1)
 						fkji[d.vi[l][2]] += - 1/((1+Z/rM)*(1+Z/rM));

start.job
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #SBATCH --job-name M4
-#SBATCH --nodes 4
+#SBATCH --nodes 1
 #SBATCH --ntasks-per-node 40
 #SBATCH --mem-per-cpu=3G
-#SBATCH --time=3:00:00
-#SBATCH --partition=med
+#SBATCH --time=48:00:00
+#SBATCH --partition=long
 #SBATCH --error=M4_%J.err
 #SBATCH --output=M4_%J.out
 